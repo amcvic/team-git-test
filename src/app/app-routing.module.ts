2a0000179b5ee429f5cd4e999bd17f7c440c8129
austin <austinmcvicker@twh-mbp.local>
2019-08-10 11:01:20 -0400

    add austin view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyViewComponent } from './my-view/my-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MyViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
